55205a30ebe353d0f4f975028fa9106c27dad498
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2024-05-14 09:18:47 -0300

    Hello World

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56001a85dc1d38fb9a0558177a9aa768dff62263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da1ef2a9dcd1174e2b9059ca2937af62471b726")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
