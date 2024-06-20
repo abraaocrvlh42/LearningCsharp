af5c804cc8da8b7c239262cc965da7f2f003d405
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2024-06-20 13:30:28 -0300

    Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,14 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        /*
+        
         string[] fraudulentOrderIDs = new string[3];
 
         fraudulentOrderIDs[0] = "A123";
         fraudulentOrderIDs[1] = "B456";
         fraudulentOrderIDs[2] = "C789";
         // fraudulentOrderIDs[3] = "D000";
-        */
 
         string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
